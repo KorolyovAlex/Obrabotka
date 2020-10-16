1adb44194d2240868e3a300d1db97058a2fda052
KorolyovAlex <korolev.prostak@gmail.com>
2020-10-16 13:49:58 +0300

    Initial commit

---
+++ Lab1/Lab1/EntryPoint.cs
@@ -0,0 +1,28 @@
+﻿using System.Drawing;
+
+
+namespace Lab1
+{
+    class EntryPoint
+    {
+        static void Main(string[] args)
+        {
+            Bitmap firstImage = new Bitmap(Properties.Resources.SadChelDepressed);
+
+            Bitmap newImage = ImageHandler.MultByConst(firstImage, 2);
+            newImage.Save("MultByConst.bmp");
+
+            newImage = ImageHandler.AddConst(firstImage, 33);
+            newImage.Save("AddConst.bmp");
+
+            newImage = ImageHandler.LogTransform(firstImage);
+            newImage.Save("LogTranform.bmp");
+
+            newImage = ImageHandler.PowerTransform(firstImage, 3);
+            newImage.Save("PowerTransform.bmp");
+
+            newImage = ImageHandler.TransformToNegative(firstImage);
+            newImage.Save("Negative.bmp");
+        }
+    }
+}
---
+++ Lab1/Lab1/ImageHandler.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Drawing;
+
+namespace Lab1
+{
+    public static class ImageHandler
+    {
+        private const int MAX_RGB_VALUE = 255;
+        private const int MIN_RGB_VALUE = 0;
+
+        public static Bitmap AddConst(Bitmap image, int term)
+        {
+            for (int width = 0; width < image.Size.Width; width++)
+            {
+                for (int height = 0; height < image.Size.Height; height++)
+                {
+                    Color pixel = image.GetPixel(width, height);
+                    int alpha = pixel.A;
+                    int red = CheckValue(pixel.R + term);
+                    int green = CheckValue(pixel.G + term);
+                    int blue = CheckValue(pixel.B + term);
+
+                    Color newColor = Color.FromArgb(alpha, red, green, blue);
+                    image.SetPixel(width, height, newColor);
+                }
+            }
+            return image;
+        }
+
+        public static Bitmap MultByConst(Bitmap image, int multiplier)
+        {
+            for (int width = 0; width < image.Size.Width; width++)
+            {
+                for (int height = 0; height < image.Size.Height; height++)
+                {
+                    Color pixel = image.GetPixel(width, height);
+                    int alpha = pixel.A;
+                    int red = CheckValue(pixel.R * multiplier);
+                    int green = CheckValue(pixel.G * multiplier);
+                    int blue = CheckValue(pixel.B * multiplier);
+
+                    Color newColor = Color.FromArgb(alpha, red, green, blue);
+                    image.SetPixel(width, height, newColor);
+                }
+            }
+            return image;
+        }
+
+        public static Bitmap LogTransform(Bitmap image)
+        {
+            for (int width = 0; width < image.Size.Width; width++)
+            {
+                for (int height = 0; height < image.Size.Height; height++)
+                {
+                    Color pixel = image.GetPixel(width, height);
+                    int alpha = pixel.A;
+                    int red = CheckValue((int)Math.Log10(1 + pixel.R));
+                    int green = CheckValue((int)Math.Log10(1 + pixel.G));
+                    int blue = CheckValue((int)Math.Log10(1 + pixel.B));
+
+                    Color newColor = Color.FromArgb(alpha, red, green, blue);
+                    image.SetPixel(width, height, newColor);
+                }
+            }
+            return image;
+        }
+
+        public static Bitmap PowerTransform(Bitmap image, int power)
+        {
+            for (int width = 0; width < image.Size.Width; width++)
+            {
+                for (int height = 0; height < image.Size.Height; height++)
+                {
+                    Color pixel = image.GetPixel(width, height);
+                    int alpha = pixel.A;
+                    int red = CheckValue((int)Math.Pow(pixel.R, power));
+                    int green = CheckValue((int)Math.Pow(pixel.G, power));
+                    int blue = CheckValue((int)Math.Pow(pixel.B, power));
+
+                    Color newColor = Color.FromArgb(alpha, red, green, blue);
+                    image.SetPixel(width, height, newColor);
+                }
+            }
+            return image;
+        }
+
+        public static Bitmap TransformToNegative(Bitmap image)
+        {
+            for (int width = 0; width < image.Size.Width; width++)
+            {
+                for (int height = 0; height < image.Size.Height; height++)
+                {
+                    Color pixel = image.GetPixel(width, height);
+                    int alpha = pixel.A;
+                    int red = MAX_RGB_VALUE - pixel.R;
+                    int green = MAX_RGB_VALUE - pixel.G;
+                    int blue = MAX_RGB_VALUE - pixel.B;
+
+                    Color newColor = Color.FromArgb(alpha, red, green, blue);
+                    image.SetPixel(width, height, newColor);
+                }
+            }
+            return image;
+        }
+
+        private static int CheckValue(int value)
+        {
+            if(value > MAX_RGB_VALUE)
+            {
+                return MAX_RGB_VALUE;
+            }
+            if(value < MIN_RGB_VALUE)
+            {
+                return MIN_RGB_VALUE;
+            }
+
+            return value;
+        }
+    }
+}
